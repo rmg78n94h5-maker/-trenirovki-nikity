de8bae2e9e8a68e04244f3478181bd926f2dea4f
rmg78n94h5-maker <rmg78n94h5@privaterelay.appleid.com>
2026-07-18 18:02:13 +1000

    Add files via upload

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 (() => {
   'use strict';
 
-  const version = '1.7.6';
+  const version = '1.7.7';
 
   globalThis.NIKITA_APP = Object.freeze({
     version,
-    cacheName: `nikita-workouts-v${version}-sport-premium-plan`,
+    cacheName: `nikita-workouts-v${version}-plan-tab-fix`,
   });
 })();
