d2964adadb6187fb19cf98d4ae0a95e9bd71594d
rmg78n94h5-maker <rmg78n94h5@privaterelay.appleid.com>
2026-07-18 21:47:41 +1000

    Add files via upload

--- version.js
+++ version.js
@@ -1,10 +1,10 @@
 (() => {
   'use strict';
 
-  const version = '1.8.0';
+  const version = '1.8.1';
 
   globalThis.NIKITA_APP = Object.freeze({
     version,
-    cacheName: `nikita-workouts-v${version}-iron-calculator`,
+    cacheName: `nikita-workouts-v${version}-progress-filter-fix`,
   });
 })();
